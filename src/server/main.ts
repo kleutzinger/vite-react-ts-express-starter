9f869e57f09aeef8c8430737334ac432d81b60a3
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-09-06 12:00:16 -0700

    respect PORT

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = parseInt(`${process.env.PORT}`, 10) || 3000;
 
 const app = express();
 
 app.get("/message", (_, res) => res.send("Hello from express!"));
 
-ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
+ViteExpress.listen(app, PORT, () => console.log("Server is listening..."));
